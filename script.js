665064bb38d73ebc2a9fc0c99f3681835ad033f5
zul-grl <zulgerel06n@gmail.com>
2024-12-16 09:02:21 +0800

    add

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let http = require("http");
+const { faker } = require("@faker-js/faker");
+function createRandomUser() {
+  return {
+    _id: faker.string.uuid(),
+    avatar: faker.image.avatar(),
+    birthday: faker.date.birthdate(),
+    email: faker.internet.email(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    sex: faker.person.sexType(),
+    subscriptionTier: faker.helpers.arrayElement(["free", "basic", "business"]),
+  };
+}
 
+const user = createRandomUser();
+console.log(user);
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello!");
+    res.end(`${user.firstName} ${user.lastName}`);
   })
   .listen(8080);
